2dd346ce09f6213cace28b912b47781594703546
Arvind Kumar <quic_c_karvin@quicinc.com>
2024-08-29 14:24:59 +0530

    sm7435-common: Enable QSPA framework
    Change-Id: I3d4c661243327013768b0b82973dc9dbbb98b2c1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,7 +67,8 @@ BOARD_BOOTCONFIG += \
     androidboot.hardware=qcom \
     androidboot.load_modules_parallel=true \
     androidboot.memcg=1 \
-    androidboot.usbcontroller=a600000.dwc3
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.vendor.qspa=true
 
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
