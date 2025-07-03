4bb40ed4d2b2461ec6b7cdf5e40b52f962d095d9
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-07-03 14:25:05 +0000

    sm7435-common: Configure NFC and Secure Element
    Change-Id: Id391267604590b6f57d0ac91886e84b4d8f4a6cb

--- common.mk
+++ common.mk
@@ -162,6 +162,31 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
     IPACM_Filter_cfg.xml
 
+# NFC
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    Tag
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
+
+ifeq ($(TARGET_USES_NXP_NFC),true)
+  PRODUCT_PACKAGES += \
+      android.hardware.nfc-service.nxp \
+      nqnfcinfo
+endif
+ifeq ($(TARGET_USES_ST_NFC),true)
+  PRODUCT_PACKAGES += \
+      android.hardware.nfc-service.st
+endif
+
 # Overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -176,6 +201,12 @@ PRODUCT_PACKAGES += \
     WifiResCommon \
     FrameworksResTarget \
     WifiResTarget
+
+# Secure Element
+ifeq ($(TARGET_USES_ST_NFC),true)
+  PRODUCT_PACKAGES += \
+      android.hardware.secure_element@1.2-service-gto
+endif
 
 # Telephony
 PRODUCT_PACKAGES += \
